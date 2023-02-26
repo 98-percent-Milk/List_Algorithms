bc1d5404a52fc9f5c5636625a62fb965c8bbb495
98-percent-Milk <mjantsn@my.bcit.ca>
2023-02-25 18:31:14 -0800

    New array problems

--- funcs/array.py
+++ funcs/array.py
@@ -1,6 +1,3 @@
-from random import choices
-
-
 def twoSum(nums: list[int], target: int) -> list[int]:
     """Given an array of intergers, return indices of the two numbers 
     such that they add up to a specific target.
@@ -154,17 +151,131 @@ def isValidSudoku(board: list[list[str]]) -> bool:
     return True
 
 
+def logestConsecutiveSequence(nums: list[int]) -> int:
+    """given an unsorted array of integers nums, return the length of the 
+    longest consecutive elements sequence.
+
+    Args:
+        nums (list[int]): list of unsorted integers.
+
+    Returns:
+        int: length of the longest consecutive elements sequence.
+    """
+    # Runtime complexity: O(n)
+    # Space complexity: O(n)
+    hashMap = set(nums)
+    longest = 0
+    for num in nums:
+        if (num - 1) not in hashMap:
+            l = 0
+            while (num + (l := l + 1)) in hashMap:
+                pass
+            longest = [l, longest][l < longest]
+    return longest
+
+
+def maxArea(height: list[int]) -> int:
+    """Given n non-negative integers a1, a2, ..., an , where each represents a
+    line height[i] at coordinate (i, height[i]).
+    Find two lines that together with the x-axis form a container, 
+    such that the container contains the most water.
+
+    Args:
+        height (list[int]): vertical height of each line.
+
+    Returns:
+        int: Maximum area of the container.
+    """
+    # Runtime complexity: O(n)
+    # Space complexity: O(1)
+    lp, rp = 0, len(height) - 1
+    maxArea = 0
+    while lp < rp:
+        maxArea = max(maxArea, min(height[lp], height[rp]) * (rp - lp))
+        (lp := lp + 1) if height[lp] < height[rp] else (rp := rp - 1)
+    return maxArea
+
+
+def threeSum(nums: list[int], target: int = 0) -> list[list[int]]:
+    """Given an integer array nums, return all the triplets that adds up to
+    target value.
+
+    Args:
+        nums (list[int]): list of integers
+        target (int, optional): Target value. Defaults to 0.
+
+    Returns:
+        list[list[int]]: list of triplets that adds up to target value.
+    """
+    nums = sorted(nums)  # Sorting will remove duplicates (results)
+    triplets = []
+    for i, a in enumerate(nums):
+        if i > 0 and a == nums[i - 1]:
+            continue
+        lp, rp = i + 1, len(nums) - 1
+        while lp < rp:
+            target = a + nums[lp] + nums[rp]
+            if target > 0:
+                rp -= 1
+            elif target < 0:
+                lp += 1
+            else:
+                triplets += [[a, nums[lp], nums[rp]]]
+                lp += 1
+                while nums[lp] == nums[lp - 1] and lp < rp:
+                    lp += 1
+    return triplets
+
+
+def nextPermutation(nums: list[int]) -> list[int]:
+    """Given an array nums of distinct integers, return a next permutation
+    which is lexicographically greater than the current permutation.
+
+    Args:
+        nums (list[int]): list of distinct integers.
+
+    Returns:
+        list[int]: lexically greater nextc permutation.
+    """
+    # If length of nums is less than or equal to 2, then reverse the list.
+    if (l := len(nums)) <= 2:
+        return nums.reverse()
+    p = l - 1
+    # Find the first index p such that nums[p] < nums[p + 1]
+    while (p := p - 1) >= 0 and nums[p] >= nums[p + 1]:
+        pass
+    # If p < 0, then reverse the list. (Last lexicographical permutation of nums)
+    if p < 0:
+        return nums.reverse()
+    # swap nums[p] with the smallest number greater than nums[p] to the right of p.
+    for i in range(l - 1, p, -1):
+        if nums[p] < nums[i]:
+            nums[p], nums[i] = nums[i], nums[p]
+            break
+    # Reverse the list from p + 1 to the end.
+    nums[p + 1:] = nums[:(p):-1]
+
+
 def main():
-    board = [["5", "3", ".", ".", "7", ".", ".", ".", "."],
-             ["6", ".", ".", "1", "9", "5", ".", ".", "."],
-             [".", "9", "8", ".", ".", ".", ".", "6", "."],
-             ["8", ".", ".", ".", "6", ".", ".", ".", "3"],
-             ["4", ".", ".", "8", ".", "3", ".", ".", "1"],
-             ["7", ".", ".", ".", "2", ".", ".", ".", "6"],
-             [".", "6", ".", ".", ".", ".", "2", "8", "."],
-             [".", ".", ".", "4", "1", "9", ".", ".", "5"],
-             [".", ".", ".", ".", "8", ".", ".", "7", "9"]]
-    isValidSudoku(board)
+    # [0, 1, 2, 10, 9, 5, 4]
+    # [0, 1, 4, 10, 9, 5, 2]
+    # [0, 1, 4, 2, 5, 9, 10]
+    # Desired => [0, 1, 2, 4, 5, 9, 10]
+    # nums = [0, 1, 2, 10, 9, 5, 4]
+    nums = [1, 3, 2]
+    l = len(nums)
+    p = l - 2
+    while p >= 0 and nums[p] >= nums[p + 1]:
+        p -= 1
+    if p < 0:
+        nums.reverse()
+        print(nums)
+        return
+    for i in range(l - 1, p, -1):
+        if nums[p] < nums[i]:
+            nums[p], nums[i] = nums[i], nums[p]
+    nums[p + 1:] = reversed(nums[p + 1:])
+    print(nums)
 
 
 if __name__ == "__main__":
